06e1f9575cc8a9bb6648fbabeb05a71b761e39dc
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-05-11 13:07:58 +0300

    add bg and Login page style

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,53 @@
+// import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100">
+        <h1 className="text-3xl font-semibold text-center text-white">
+          Login <span className="text-orange-400   ">ChatApp</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text text-gray-200">
+                Username
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Username..."
+              className="w-full input input-bordered h-10 text-gray-100"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text text-gray-200">
+                Password
+              </span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password..."
+              className="w-full input input-bordered h-10 text-gray-100"
+            />
+            <a
+              href="#"
+              className="text-sm text-gray-200 hover:underline hover:text-orange-400 mt-2 inline-block"
+            >
+              {"Don't"} have an account?
+            </a>
+
+            <div>
+              <button className="btn btn-block btn-sm mt-2">Login</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
